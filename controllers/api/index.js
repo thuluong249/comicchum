06ca991b4e02262d394eb22d4276dfa4cf661ce0
Pratik Patel <pratikpatel_85@yahoo.com>
2021-07-06 15:27:46 -0400

    fixed routes to show new file names

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const comicRoutes = require('./comicRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/comics', comicRoutes);
 
 module.exports = router;
