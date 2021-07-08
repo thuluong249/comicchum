fd29aa44b6327ed01b247a85e094b21be91d375c
Pratik Patel <pratikpatel_85@yahoo.com>
2021-07-08 11:29:09 -0400

    fixed spelling errors

--- models/Comic.js
+++ models/Comic.js
@@ -5,46 +5,43 @@ class Comics extends Model {}
 
 //will need to change values and names below
 
-Comics.init(
-  {
+Comics.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     comments: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
-    main_charachter: {
-      type: DataTypes.STRING,
-      allowNull: true,
+    main_character: {
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     author: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     year: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     cover: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
-  },
-  {
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: "comics",
-  }
-);
+});
 
 module.exports = Comics;
